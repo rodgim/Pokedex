533290e7287d79f02896d7d46450d0cdc982a523
Rodrigo Gimenez <rodrigo_g90@hotmail.es>
2023-07-18 23:17:57 -0400

    Setup dagger hilt and set up the navigation

--- app/src/main/java/com/rodgim/pokedex/MainActivity.kt
+++ app/src/main/java/com/rodgim/pokedex/MainActivity.kt
@@ -8,8 +8,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.rodgim.pokedex.ui.theme.PokedexTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +29,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "pokemon_list_screen") {
+                        composable("pokemon_list_screen") {
+
+                        }
+                        composable(
+                            "pokemon_detail_screen/{dominantColor}/{pokemonName}",
+                            arguments = listOf(
+                                navArgument("dominantColor") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("pokemonName") {
+                                    type = NavType.StringType
+                                }
+                            )
+                        ) {
+                            val dominantColor = remember {
+                                val color = it.arguments?.getInt("dominantColor")
+                                color?.let { Color(it) } ?: Color.White
+                            }
+                            val pokemonName = remember {
+                                it.arguments?.getString("pokemonName")
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PokedexTheme {
-        Greeting("Android")
     }
 }
