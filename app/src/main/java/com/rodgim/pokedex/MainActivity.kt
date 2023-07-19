b8ca18cc1a5e4de583df0a52a3d06c42dd10b4a0
Rodrigo Gimenez <rodrigo_g90@hotmail.es>
2023-07-19 14:38:01 -0400

    Create the search bar for the Pokemon list screen

--- app/src/main/java/com/rodgim/pokedex/MainActivity.kt
+++ app/src/main/java/com/rodgim/pokedex/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.rodgim.pokedex.ui.pokemonlist.PokemonListScreen
 import com.rodgim.pokedex.ui.theme.PokedexTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     val navController = rememberNavController()
                     NavHost(navController = navController, startDestination = "pokemon_list_screen") {
                         composable("pokemon_list_screen") {
-
+                            PokemonListScreen(navController = navController)
                         }
                         composable(
                             "pokemon_detail_screen/{dominantColor}/{pokemonName}",
