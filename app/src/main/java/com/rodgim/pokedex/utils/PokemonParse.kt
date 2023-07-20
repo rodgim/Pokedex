603d55b2e23a18fc4e0e4b439e32cc51cc26ae46
Rodrigo Gimenez <rodrigo_g90@hotmail.es>
2023-07-19 23:44:13 -0400

    Add to the detail screen the weight, height, and type of the Pokemon

---
+++ app/src/main/java/com/rodgim/pokedex/utils/PokemonParse.kt
@@ -0,0 +1,55 @@
+package com.rodgim.pokedex.utils
+
+import androidx.compose.ui.graphics.Color
+import com.rodgim.pokedex.data.remote.responses.PokemonStat
+import com.rodgim.pokedex.data.remote.responses.PokemonType
+import com.rodgim.pokedex.ui.theme.*
+import java.util.Locale
+
+fun parseTypeToColor(type: PokemonType): Color {
+    return when(type.type.name.lowercase(Locale.ROOT)) {
+        "normal" -> TypeNormal
+        "fire" -> TypeFire
+        "water" -> TypeWater
+        "electric" -> TypeElectric
+        "grass" -> TypeGrass
+        "ice" -> TypeIce
+        "fighting" -> TypeFighting
+        "poison" -> TypePoison
+        "ground" -> TypeGround
+        "flying" -> TypeFlying
+        "psychic" -> TypePsychic
+        "bug" -> TypeBug
+        "rock" -> TypeRock
+        "ghost" -> TypeGhost
+        "dragon" -> TypeDragon
+        "dark" -> TypeDark
+        "steel" -> TypeSteel
+        "fairy" -> TypeFairy
+        else -> Color.Black
+    }
+}
+
+fun parseStatToColor(stat: PokemonStat): Color {
+    return when(stat.stat.name.lowercase(Locale.ROOT)) {
+        "hp" -> HPColor
+        "attack" -> AtkColor
+        "defense" -> DefColor
+        "special-attack" -> SpAtkColor
+        "special-defense" -> SpDefColor
+        "speed" -> SpdColor
+        else -> Color.White
+    }
+}
+
+fun parseStatToAbbr(stat: PokemonStat): String {
+    return when(stat.stat.name.lowercase(Locale.ROOT)) {
+        "hp" -> "HP"
+        "attack" -> "Atk"
+        "defense" -> "Def"
+        "special-attack" -> "SpAtk"
+        "special-defense" -> "SpDef"
+        "speed" -> "Spd"
+        else -> ""
+    }
+}
